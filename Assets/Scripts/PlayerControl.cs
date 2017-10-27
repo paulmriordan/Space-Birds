1c2a4626a78fb6398b3f65d1c3d2ac1be420b970
Paul Riordan <paulmriordan@hotmail.com>
2017-10-27 12:52:20 +0100

    Initial commit - character force up control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControl : MonoBehaviour {
+
+    [SerializeField]
+    float ForceUp = 1.0f;
+
+    Rigidbody2D m_rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            m_rigidBody.AddForce(Vector2.up * ForceUp);
+	}
+}
