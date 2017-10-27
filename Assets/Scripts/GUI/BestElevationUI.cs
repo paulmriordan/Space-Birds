b193006a3a19f826d8bd482145c4652bfe5b6535
Paul Riordan <paulmriordan@hotmail.com>
2017-10-27 17:39:01 +0100

    Elevation UI

---
+++ Assets/Scripts/GUI/BestElevationUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections;
+
+
+public class BestElevationUI : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_objectToTrack = null;
+
+    private float m_best;
+    private Text m_text;
+
+    void Start()
+    {
+        m_best = PlayerPrefs.GetFloat("bestElevation", 0);
+        m_text = GetComponent<Text>();
+    }
+    
+    void Update()
+    {
+        if (m_objectToTrack.transform.position.y > m_best)
+        {
+            m_best = m_objectToTrack.transform.position.y;
+            PlayerPrefs.SetFloat("bestElevation", m_best);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Best: ");
+        sb.Append(m_best.ToString("0"));
+        sb.Append("m");
+        m_text.text = sb.ToString();
+    }
+}
---
+++ Assets/Scripts/GUI/ElevationUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections;
+
+
+public class ElevationUI : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_objectToTrack;
+
+    private Text m_text;
+
+    void Start()
+    {
+        m_text = GetComponent<Text>();
+    }
+    
+    void Update()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Elevation: ");
+        sb.Append(m_objectToTrack.transform.position.y.ToString("0"));
+        sb.Append("m");
+        m_text.text = sb.ToString();
+    }
+}
