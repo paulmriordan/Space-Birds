b6d5d4b978c9c957b8d8c2dea1c4a7993fce1fee
Paul Riordan <paulmriordan@hotmail.com>
2017-10-27 17:10:33 +0100

    Basic animator for bird

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerAnimationController : MonoBehaviour {
+
+    public float FlyingUpTreshold = 1.0f;
+
+    private Animator m_animator;
+    private Rigidbody2D m_rigidbody2D;
+
+    private int m_flyingUpKey;
+    
+	void Start () {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        m_flyingUpKey = Animator.StringToHash("FlyingUp");
+    }
+	
+	void Update ()
+    {
+        m_animator.SetBool(m_flyingUpKey, m_rigidbody2D.velocity.y > FlyingUpTreshold); 
+	}
+}
