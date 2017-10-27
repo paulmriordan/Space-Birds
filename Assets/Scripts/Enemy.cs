eb373fc3450fef4e2ee19fae436348fc153af36c
Paul Riordan <paulmriordan@hotmail.com>
2017-10-27 16:01:53 +0100

    Recycle enemies when passed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,23 @@ public class Enemy : MonoBehaviour {
 
     public event Action<Enemy> OnEnemyDead;
 
-	void Start () {
-		
-	}
+    [SerializeField]
+    public float KillAfterDistancePassed = 20.0f;
+
+    private Transform m_camera;
+
+    void Start ()
+    {
+        m_camera = Camera.main.transform;
+    }
 	
 	void Update ()
     {
-		
-	}
+	    if ((m_camera.transform.position.y - this.transform.position.y) > KillAfterDistancePassed)
+        {
+            if (OnEnemyDead != null)
+                OnEnemyDead(this);
+        }
+
+    }
 }
